90c7034c19cc4695533f6e316c2dd5427735444e
maciatko62 <ania.soliwoda62@gmail.com>
2022-09-17 22:15:14 +0200

    Added slider

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuiteGame()
+    {
+        Debug.Log("Quit :(");
+        Application.Quit();
+    }
+
+    /*
+    public void SoundMenuOn()
+    {
+        PlayerPrefs.SetInt("MenuSound", 1);
+    }
+    public void SoundMenuOff()
+    {
+        PlayerPrefs.SetInt("MenuSound", 0);
+    }
+    */
+}
---
+++ Assets/Scripts/MenuSlider.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSlider : MonoBehaviour
+{
+    public Slider slider;
+    public Text sliderText;
+
+    private void Update()
+    {
+        GameObject.Find("Number").GetComponent<Text>().text = "" + slider.value;
+        //sliderText.text = slider.value.ToString();
+    }
+}
