033516e9177bcbac266b34f5105bbf5881759a18
maciatko62 <ania.soliwoda62@gmail.com>
2022-09-19 16:28:11 +0200

    Added spawning chests

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     public GameObject chest;
-    public Transform pos;
+    //public Vector3 pos;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(chest, pos.position, pos.rotation);
+        for(int i = 0; i < 10; i++)
+        {
+            Instantiate(chest, transform.position, transform.rotation);
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+
+        //chest = new GameObject("ch");
+        //chest.gameObject.GetComponentInParent = this.gameObject;
+
+        //chest.transform.position = new Vector3(3, 5, 0);
+
+        //GameObject newObject = new GameObject("ab");
+        //Instantiate(chest, pos.position, pos.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
