b46e6f5f9438b7e1d6b15980a86f047f1130d564
maciatko62 <ania.soliwoda62@gmail.com>
2022-09-19 18:39:39 +0200

    Updated numbers on the cheasts

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -11,33 +11,17 @@ public class Chest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 1; i <= 15; i++)
+        for(int i = 0; i < PlayerPrefs.GetInt("listCount"); i++)
         {
             chest.name = "Chest" + i;
             Instantiate(chest, transform.position, transform.rotation);
 
             transform.position = new Vector3(transform.position.x + 0.6f, transform.position.y, transform.position.z);
-            //chest = GameObject.Find("/Chest0/CanvasSW/Text (TMP)");
-            //GameObject.Find("Chest0(Clone)/CanvaSW/Text (TMP)").GetComponent<TMPro.TextMeshProUGUI>().text = "" + i;
-            //GameObject.Find("Chest5(Clone)").GetComponent<TMPro.TextMeshProUGUI>().text = "" + i;
 
-            //GameObject.Find("Text (TMP)").GetComponent<TMPro.TextMeshProUGUI>().text = "" + i;
-            GameObject.FindGameObjectWithTag("1").GetComponent<TMPro.TextMeshProUGUI>().text = "" + i;
+            GameObject.FindGameObjectWithTag("1").GetComponent<TMPro.TextMeshProUGUI>().text = "" + PlayerPrefs.GetInt("list_" + i);
             GameObject.FindGameObjectWithTag("1").tag = "2";
         }
         
-
-        //chest = new GameObject("ch");
-        //chest.gameObject.GetComponentInParent = this.gameObject;
-
-        //chest.transform.position = new Vector3(3, 5, 0);
-
-        //GameObject newObject = new GameObject("ab");
-        //Instantiate(chest, pos.position, pos.rotation);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
 }
