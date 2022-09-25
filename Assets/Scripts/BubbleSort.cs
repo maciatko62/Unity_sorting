63641e0c73f232117967fff99af36b68bc17f4c9
maciatko62 <ania.soliwoda62@gmail.com>
2022-09-25 14:50:19 +0200

    Added number od steps

--- Assets/Scripts/BubbleSort.cs
+++ Assets/Scripts/BubbleSort.cs
@@ -9,6 +9,8 @@ public class BubbleSort : MonoBehaviour
     public PickUp pickUp;
     public AiScript ai;
     public MenuScript menu;
+    private bool stopTime =false;
+    private int numberOfSteps = 0;
 
     void Start()
     {
@@ -40,24 +42,29 @@ public class BubbleSort : MonoBehaviour
                 yield return new WaitForSeconds(1.5f);
                 pickUp.PickUpL();
                 pickUp.PickUpR();
+                //numberOfSteps++;
+                //GameObject.Find("TextN").GetComponent<Text>().text = "Number of steps: " + numberOfSteps;
                 yield return new WaitForSeconds(0.5f);
 
                 if (list[i] > list[i + 1])
                 {
+                    numberOfSteps++;
+                    GameObject.Find("TextN").GetComponent<Text>().text = "Number of steps: " + numberOfSteps;
                     pickUp.PickUpT();
                     yield return new WaitForSeconds(0.5f);
                     pickUp.RightToLeft();
                     yield return new WaitForSeconds(0.5f);
                     pickUp.LeftToRight();
                     yield return new WaitForSeconds(0.5f);
-
                     
+
                     int tmp = list[i];
                     list[i] = list[i + 1];
                     list[i + 1] = tmp;
                 }
                 pickUp.DropObjectL();
                 pickUp.DropObjectR();
+                
                 yield return new WaitForSeconds(0.5f);
             }
             n--;
@@ -66,6 +73,7 @@ public class BubbleSort : MonoBehaviour
         }
         while (n > 1);
         menu.FinalMessageOn();
+        stopTime = true;
         for (int i = 0; i < listt.Count; i++)
         {
             //Debug.Log(listt[i]);
@@ -75,13 +83,16 @@ public class BubbleSort : MonoBehaviour
     IEnumerator StartTime()
     {
         yield return new WaitForSeconds(1.5f);
-        GameObject.Find("TextN").GetComponent<Text>().text = "Number of steps: 0";
-        GameObject.Find("TextT").GetComponent<Text>().text = "Sorting time: 00:00";
+        //GameObject.Find("TextN").GetComponent<Text>().text = "Number of steps: 0";
+        //GameObject.Find("TextT").GetComponent<Text>().text = "Sorting time: 00:00";
         for(int j = 0; j < 10; j++)
         {
             for (int i = 0; i < 60; i++)
             {
-                
+                if(stopTime == true)
+                {
+                    break;
+                }
                 if (i < 10)
                 {
                     GameObject.Find("TextT").GetComponent<Text>().text = "Sorting time: 0" + j +":0" + i;
